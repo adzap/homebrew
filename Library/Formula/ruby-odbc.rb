06c9bbd9c9450630ea65b918cefb0e56b983420b
Hugh Evans <hugh@artpop.com.au>
2009-11-05 13:42:17 +1100

    RubyODBC 0.9997: http://www.ch-werner.de/rubyodbc/

---
+++ Library/Formula/ruby-odbc.rb
@@ -0,0 +1,13 @@
+require 'brewkit'
+
+class RubyOdbc < Formula
+  @url='http://www.ch-werner.de/rubyodbc/ruby-odbc-0.9997.tar.gz'
+  @homepage='http://www.ch-werner.de/rubyodbc/'
+  @md5='36d21519795c3edc8bc63b1ec6682b99'
+
+  def install
+    system 'ruby extconf.rb', '--enable-dlopen'
+    system 'make'
+    system 'make install'
+  end
+end
